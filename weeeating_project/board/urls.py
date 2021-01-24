c88c27e5d12d2a654b000a3754a755cd6dba1c08
김예진 <jin11241124@gmail.com>
2021-01-24 23:28:15 +0900

    [UPDATE] 상세페이지 작성중

--- weeeating_project/board/urls.py
+++ weeeating_project/board/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import BoardMainView, BoardPostView
+from .views import BoardView, BoardDetailView
 
 urlpatterns = [
-    path('', BoardMainView.as_view()),
-    path('/posting', BoardPostView.as_view())
+    path('', BoardView.as_view()), #조회,작성 
+    path('/detail/<int:board_id>', BoardDetailView.as_view()) #상세페이지 조회
 ]
--- weeeating_project/board/views.py
+++ weeeating_project/board/views.py
@@ -8,31 +8,35 @@ from django.views import View
 from .models import Board, BoardComment
 
 #깃
-class BoardMainView(View):
+class BoardView(View):
     #@decorator
     def get(self, request):
-        recent_list = list(Board.objects.all().select_related('writer').prefetch_related('boardcomment_set'))[-5:]
+        offset = 0
+        limit = 5
+        recent_list = list(Board.objects.all().select_related('writer').prefetch_related('boardcomment_set'))
         #user_id = request.user.id
+
+        total_board = len(Board.objects.all())
 
         board_list =[{
             'id' : board.id,
             'title' : board.title,
+            'writer_id' : board.writer_id,
             'writer' : board.writer.name,
-            'created_at' : board.created_at,
+            'created_at' : board.created_at.strftime("%Y-%m-%d %I:%M:%S"),
             'comments' : len([par.comment for par in board.boardcomment_set.filter(board_id = board.id)])
-        } for board in recent_list]
-
-        return JsonResponse({'board_list' : board_list}, status=200)
+        } for board in recent_list][::-1][offset:offset+limit]
 
 
-class BoardPostView(View):
-    #로그인 한 유저만 작성 가능(조건문 추가해야함)
+        return JsonResponse({'board_list' : board_list, "total_board" : total_board}, status=200)
+
     #@decorator
     def post(self, request):
         #user_id = request.user.id
         offset = 0
         limit = 5
         data = json.loads(request.body)
+        print(data)
 
         new_board = Board.objects.create(
             writer_id = 1,
@@ -41,19 +45,30 @@ class BoardPostView(View):
             content = data['content']
         )
 
-        recent_list = list(Board.objects.all().select_related('writer').prefetch_related('boardcomment_set__comment'))
+        #new_list = board_list()
 
-        print(recent_list)
+#        board_list = list(Board.objects.all().select_related('writer').prefetch_related('boardcomment_set__comment'))
+#        new_list = [{
+#            'id' : board.id,
+#            'title' : board.title,
+#            'writer_id' : board.writer_id,
+#            'writer' : board.writer.name,
+#            'created_at' : board.created_at.strftime("%Y-%m-%d %I:%M:%S"),
+#            'comments' : len([par.comment for par in board.boardcomment_set.filter(board_id = board.id)])
+#        } for board in board_list]#[offset:offset+limit]
 
-        new_list = [{
-            'id' : board.id,
-            'title' : board.title,
-            'writer_id' : board.writer_id,
-            'writer' : board.writer.name,
-            'created_at' : board.created_at.strftime("%Y-%m-%d %I:%M:%S"),
-            'comments' : len([par.comment for par in board.boardcomment_set.filter(board_id = board.id)])
-        } for board in recent_list][offset:offset+limit]
+        return JsonResponse({'Message' : 'SUCCESS'}, status=201)
 
-        return JsonResponse({'new_list' : new_list}, status=201)
 
+
+class BoardDetailView(View):
+    #@decorator
+    def get(sefl, request, board_id): #상세페이지 조회 
+        #user_id = request.user.id
+
+        board_info = Board.objects.filter(id = board_id).values()
+
+
+
+        return JsonResponse({'board_info' : board_info} , status=200)
 
