73978fd79fac9dd6c9b5aaa2fd8c026de12d892c
김예진 <jin11241124@gmail.com>
2021-01-25 17:46:13 +0900

    [UPDATE] 게시글 댓글 수정 삭제 기능

--- weeeating_project/board/views.py
+++ weeeating_project/board/views.py
@@ -11,7 +11,7 @@ from user.models import User
 #깃
 class BoardView(View):
     #@decorator
-    def get(self, request):
+    def get(self,request):
         offset = 0
         limit = 5
         recent_list = list(Board.objects.all().select_related('writer').prefetch_related('boardcomment_set'))
@@ -32,7 +32,7 @@ class BoardView(View):
         return JsonResponse({'board_list' : board_list, "total_board" : total_board}, status=200)
 
     #@decorator
-    def post(self, request):
+    def post(self,request):
         #user_id = request.user.id
         offset = 0
         limit = 5
@@ -40,45 +40,70 @@ class BoardView(View):
 
         new_board = Board.objects.create(
             writer_id = 1,
-            #writer = User.objects.get(id=user_id),
+            #writer_id = User.objects.get(id=user_id).id,
             title = data['title'],
             content = data['content']
         )
 
-        return JsonResponse({'Message' : 'SUCCESS'}, status=201)
+        return JsonResponse({'MESSAGE' : 'SUCCESS'}, status=201)
 
 
 
-class BoardDetailView(View):
+class BoardDetailView(View): #상세페이지 조회,수정,삭제
     #@decorator
-    def get(self, request, board_id): #상세페이지 조회 
+    def get(self,request,board_id):
         #user_id = request.user.id
 
         board_info = Board.objects.filter(id = board_id).select_related('writer').prefetch_related('boardcomment_set').all()
+
         comments_list = BoardComment.objects.filter(board_id = board_id).select_related('writer')
 
-        print(comments_list)
-
-        board_detail =[[{
+        board_detail =[{
             'title' : board.title,
             'writer_id' : board.writer.id,
             'writer' : board.writer.name,
             'content' : board.content,
             'create_at' : board.created_at.date()
-        } for board in board_info],
-        [{
-            'count_comment' : len(comments_list),
+        } for board in board_info]
+
+        count_comments = len(comments_list)
+
+        board_comments = [{
             'comment_id' : comment.id,
             'comment_writer' : comment.writer.name,
             'comment_writer_id' : comment.writer.id,
             'comment_content' : comment.comment,
             'comment_created_at' : comment.created_at.strftime("%Y-%m-%d %I:%M:%S")}
-            for comment in comments_list]]
+            for comment in comments_list]
 
-        return JsonResponse({'board_info' : board_detail} , status=200)
+        return JsonResponse({'board_info':board_detail, 'count_comments':count_comments, 'board_comments':board_comments} , status=200)
 
+    def patch(self,request,board_id):
+        #user_id = request.user_id
+        data = json.loads(request.body)
+        user_id = 1
 
-class BoardCommentView(View):
+        board = Board.objects.get(id=board_id)
+
+        if board.writer_id == user_id :
+            Board.objects.filter(id=board_id).update(
+                title = data['title'],
+                content = data['content'])
+            return JsonResponse({'MESSAGE' : 'UPDATE_SUCCESS'}, status=200)
+        return JsonResponse({'MESSAGE' : 'ACCESS_DENIED'}, status=403)
+
+    def delete(self,request,board_id):
+        #user_id = request.user_id
+        user_id = 1
+
+        board = Board.objects.get(id=board_id)
+
+        if board.writer_id == user_id :
+            board.delete()
+            return JsonResponse({'MESSAGE' : 'DELETE_SUCCESS'}, status=200)
+        return JsonResponse({'MESSAGE' : 'ACCESS_DENIED'}, status=403)
+
+class BoardCommentView(View): #게시글 댓글(생성,수정,삭제)
     #@decorator
     def post(self,request,board_id):
         #user_id = request.user.id
@@ -91,5 +116,36 @@ class BoardCommentView(View):
             comment = data['comment']
         )
 
-        return JsonResponse({'MESSAGE' : "COMMENT_CREATE_SUCCESS"},status=201)
+        return JsonResponse({'MESSAGE' : 'COMMENT_CREATE_SUCCESS'},status=201)
+
+    #@decorator
+    def patch(self,request,board_id,comment_id):
+        #user_id = request.user.id
+        user_id = 1
+        data = json.loads(request.body)
+        comment_writer_id = BoardComment.objects.get(id = comment_id).writer_id
+
+        if comment_writer_id == user_id :
+            BoardComment.objects.filter(id=comment_id).update(
+                comment = data['comment']
+            )
+
+            return JsonResponse({'MESSAGE' : 'UPDATE_SUCCESS'}, status=200)
+        return JsonResponse({'MESSAGE' : 'ACCESS_DENIED'}, status=403)
+
+    def delete(self,request,board_id,comment_id):
+        #user_id = request.user.id
+        user_id = 1
+
+        comment = BoardComment.objects.get(id = comment_id)
+
+        if comment.writer_id == user_id :
+            comment.delete()
+
+            return JsonResponse({'MESSAGE' : 'DELETE_SUCCESS'}, status=200)
+        return JsonResponse({'MESSAGE' : 'ACCESS_DENIED'}, status=403)
+
+
+
+
 
